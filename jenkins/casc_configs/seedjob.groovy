5df2a12d327e149fc8491b046abf11d83459b284
Vladislav Medvedev <vlad-medvedev-99@inbox.ru>
2023-04-05 14:19:12 +0300

    WIP

--- jenkins/casc_configs/seedjob.groovy
+++ jenkins/casc_configs/seedjob.groovy
@@ -50,7 +50,7 @@ multibranchPipelineJob('App/Build(multibranch)') {
             source {
                 github {
                     id('1234567') // IMPORTANT: use a constant and unique identifier
-                    credentialsId('github_ssh')
+                    credentialsId('github_ssh2')
                     repoOwner('Phaeton-vlm')
                     repository('todo-vue')
                     repositoryUrl('https://github.com/Phaeton-vlm/todo-vue.git')
@@ -82,5 +82,26 @@ multibranchPipelineJob('App/Build(multibranch)') {
         discardOldItems {
             numToKeep(15)
         }
+    }
+}
+
+pipelineJob('App/Deploy') {
+    description('<div style="border-radius:10px; text-align: center; font-size:120%; padding:15px; background-color: powderblue;">Deploy application pipeline</div>')
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        credentials('github_ssh2')
+                        github('Phaeton-vlm/todo-vue')
+                    }
+                    branch('master')
+                }
+            }
+            scriptPath('Jenkinsfile_deploy')
+        }
+    }
+    parameters {
+        stringParam('D_TAG', '0.0', 'image version')
     }
 }
